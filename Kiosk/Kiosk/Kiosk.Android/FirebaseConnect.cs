f0d9a010a735b6221570990c481a04b97277696f
Trevor W <34048315+trevor34@users.noreply.github.com>
2021-12-04 23:11:20 -0800

    Very not done but I am tired

---
+++ Kiosk/Kiosk/Kiosk.Android/FirebaseConnect.cs
@@ -0,0 +1,107 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+using System.Threading.Tasks;
+using Firebase.Firestore;
+using Android.Gms.Tasks;
+
+[assembly: Dependency(typeof(Kiosk.Droid.FirebaseConnect))]
+namespace Kiosk.Droid
+{
+    class FirebaseConnect : IFirebaseConnect
+    {
+        public Task<MajorData> GetMajor(string Major)
+        {
+            var tcs = new TaskCompletionSource<MajorData>();
+
+            FirebaseFirestore.Instance
+                .Collection("pages")
+                .Document("Majors")
+                .Collection("Degrees")
+                .Document(Major)
+                .Get()
+                .AddOnCompleteListener(new OnCompleteListener(tcs));
+
+            return tcs.Task;
+        }
+
+        public Task<LinkedList<string>> GetMajorsList()
+        {
+            var tcs = new TaskCompletionSource<LinkedList<string>>();
+
+            FirebaseFirestore.Instance
+                .Collection("pages")
+                .Document("Majors")
+                .Collection("Degrees")
+                .Get()
+                .AddOnCompleteListener(new OnListComplete(tcs));
+
+            return tcs.Task;
+        }
+    }
+
+    internal class OnListComplete : Java.Lang.Object, IOnCompleteListener
+    {
+        private TaskCompletionSource<LinkedList<string>> tcs;
+
+        public OnListComplete(TaskCompletionSource<LinkedList<string>> tcs)
+        {
+            this.tcs = tcs;
+        }
+
+        public async System.Threading.Tasks.Task OnCompleteAsync(Android.Gms.Tasks.Task task)
+        {
+            if (task.IsSuccessful)
+            {
+                Java.Lang.Object result = task.Result;
+                if (result is CollectionReference col)
+                {
+                    QuerySnapshot snapshot = await col.GetSnapshotAsync();
+                    var degrees = new LinkedList<string>();
+                    foreach (DocumentReference d in col.Documents)
+                    {
+
+                    }
+
+                }
+            }
+
+            tcs.TrySetResult(default(LinkedList<string>));
+        }
+    }
+
+    internal class OnCompleteListener : Java.Lang.Object, IOnCompleteListener
+    {
+        private TaskCompletionSource<MajorData> tcs;
+
+        public OnCompleteListener(TaskCompletionSource<MajorData> tcs)
+        {
+            this.tcs = tcs;
+        }
+
+        public void OnCompleteAsync(Android.Gms.Tasks.Task task)
+        {
+            if (task.IsSuccessful)
+            {
+                Java.Lang.Object result = task.Result;
+                if (result is DocumentSnapshot doc)
+                {
+                    var major = new MajorData();
+                    major.about = doc.GetArray("about");
+                    major.campuses = doc.GetString("campuses");
+
+                }
+            }
+
+            tcs.TrySetResult(default(MajorData));
+        }
+    }
+}
---
+++ Kiosk/Kiosk/Kiosk/IFirebaseConnect.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kiosk
+{
+    public interface IFirebaseConnect
+    {
+        Task<MajorData> GetMajor(string Major);
+
+        Task<LinkedList<string>> GetMajorsList();
+    }
+}
