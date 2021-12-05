39ef22a9e5e2c11000681b5d9bb95e077b286636
skittlemassacare <zombiedude723@gmail.com>
2021-12-04 16:11:29 -0800

    added the fileIO/Kiosk database interface ,not working as of now. Still figuring out the data types required for it to take in the data correctly.

---
+++ Kiosk/KioskDatabaseFramework/MajorData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KioskDatabaseFramework
+{
+    public class MajorData
+    {
+            public string MajorName { get; set; }
+            public List<string> type { get; set; }
+            public List<string> Classes { get; set; }
+            public List<string> Professors { get; set; }
+            public List<string> campuses { get; set; }
+            public List<string> about { get; set; }
+
+    }
+}
