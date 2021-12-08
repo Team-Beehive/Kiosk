3d6944b91110cddb8bb371af2290264d02aa0932
Trevor W <34048315+trevor34@users.noreply.github.com>
2021-12-08 00:20:08 -0800

    Image cropping is probably done

--- Kiosk/Kiosk/Kiosk/CroppedImage.cs
+++ Kiosk/Kiosk/Kiosk/CroppedImage.cs
@@ -10,19 +10,143 @@ namespace Kiosk
     {
         public CroppedImage()
         {
-            internalImage = new Image 
-            {
-                
-            };
+            internalImage = new Image();
+
+            IsClippedToBounds = true;
+            CropAspect = S.Square;
+            Orientation = O.Horizontal;
             VerticalOptions = LayoutOptions.End;
             HorizontalOptions = LayoutOptions.End;
-            ShapeRatio = S.Square;
-            Orientation = O.Horizontal;
+
             ImageConstraintX = 0;
             ImageConstraintY = 0;
             ImageScaleX = 1;
             ImageScaleY = 1;
 
+
+            Children.Add(internalImage,
+                // These constraints are in the correct but arbitrary order
+                // XConstraint
+                Constraint.RelativeToParent((parent) =>
+                {
+                    return parent.Width * ImageConstraintX;
+                }),
+                //YConstraint
+                Constraint.RelativeToParent((parent) =>
+                {
+                    return parent.Height * ImageConstraintY;
+                }),
+                // XScale
+                Constraint.RelativeToParent((parent) =>
+                {
+                    return parent.Width * ImageScaleX;
+                }),
+                // YScale
+                Constraint.RelativeToParent((parent) =>
+                {
+                    return parent.Height * ImageScaleY;
+                })
+            );
+        }
+
+        public enum S
+        {
+            Square,
+            FourByThree,
+            SixteenByNine
+        }
+
+        private static readonly double[] doubleRatio =
+        {
+            1.0,
+            1.33,
+            1.77
+        };
+
+        public enum O
+        {
+            Vertical,
+            Horizontal
+        }
+
+        private O orientation;
+        public O Orientation
+        {
+            get => orientation;
+            set
+            {
+                orientation = value;
+                SetCropAspect(cropAspect, Width);
+            }
+        }
+
+        private S cropAspect;
+        public S CropAspect
+        {
+            get => cropAspect;
+            set
+            {
+                cropAspect = value;
+                SetCropAspect(value, Width);
+            }
+        }
+
+        private Image internalImage;
+        public ImageSource Source
+        {
+            get => internalImage.Source;
+            set => internalImage.Source = value;
+        }
+
+        private double imageX;
+        private double imageY;
+        private double scaleX;
+        private double scaleY;
+
+        public double ImageConstraintX
+        {
+            get => imageX;
+            set
+            {
+                imageX = value;
+                ConstraintChanged();
+            }
+        }
+
+        public double ImageConstraintY
+        {
+            get => imageY;
+            set
+            {
+                imageY = value;
+                ConstraintChanged();
+            }
+        }
+
+        public double ImageScaleX
+        {
+            get => scaleX;
+            set
+            {
+                scaleX = value;
+                ConstraintChanged();
+            }
+        }
+
+        public double ImageScaleY
+        {
+            get => scaleY;
+            set
+            {
+                scaleY = value;
+                ConstraintChanged();
+            }
+        }
+
+        private void ConstraintChanged()
+        {
+            // I can't seem to find a way to change this other than removing the whole image
+            Children.Remove(internalImage);
             Children.Add(internalImage,
                 // These constraints are in the correct but arbitrary order
                 // XConstraint
@@ -47,109 +171,30 @@ namespace Kiosk
                 }));
         }
 
-        
-
-        public enum S
+        protected override void OnSizeAllocated(double width, double height)
         {
-            Square,
-            FourByThree,
-            SixteenByNine
+            base.OnSizeAllocated(width, height);
+            SetCropAspect(cropAspect, width);
         }
 
-        private static readonly double[] doubleRatio =
+        private void SetCropAspect(S ratio, double width)
         {
-            1.0,
-            1.33,
-            1.77
-        };
 
-        public enum O
-        {
-            Vertical,
-            Horizontal
-        }
-
-        
-
-        public O Orientation
-        {
-            get => orientation;
-            set
-            {
-                orientation = value;
-                SetShapeRatio(shapeRatio);
-            }
-        }
-
-
-        private O orientation;
-
-        private S shapeRatio;
-        public S ShapeRatio
-        {
-            get => shapeRatio;
-            set
-            {
-                shapeRatio = value;
-                SetShapeRatio(value);
-            }
-        }
-
-        
-
-        private Image internalImage;
-        public ImageSource Source
-        {
-            get => internalImage.Source;
-            set => internalImage.Source = value;
-        }
-
-        private double imageX;
-        private double imageY;
-        private double scaleX;
-        private double scaleY;
-
-        public double ImageConstraintX 
-        { 
-            get => imageX;
-            set
-            {
-                imageX = value;
-                ConstraintChanged("ImageX");
-            };
-        }
-
-        private void ConstraintChanged(string Constraint)
-        {
-            
-            switch (Constraint)
-            {
-                case "ImageX":
-                    this.Get(;
-                    break;
-            }
-        }
-
-        public double ImageConstraintY { get => imageY; set => imageY = value; }
-        public double ImageScaleX { get => scaleX; set => scaleX = value; }
-        public double ImageScaleY { get => scaleY; set => scaleY = value; }
-
-        private void SetShapeRatio(S ratio)
-        {
             double ratioInDouble = doubleRatio[(int)ratio];
 
             switch (orientation)
             {
                 case O.Vertical:
-                    HeightRequest = Width * ratioInDouble;
+                    HeightRequest = width * ratioInDouble;
                     break;
                 case O.Horizontal:
-                    HeightRequest = Width / ratioInDouble;
+                    HeightRequest = width / ratioInDouble;
                     break;
                 default:
                     Debug.WriteLine("This isn't supposed to happen");
                     break;
             }
+
         }
     }
 }
